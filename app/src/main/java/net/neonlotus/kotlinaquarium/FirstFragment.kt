cf634ba81d5e698a7c6aa1d4be2a1865b48e24e7
Ryan T <neonxero@gmail.com>
2022-01-20 17:40:03 -0500

    second...

--- app/src/main/java/net/neonlotus/kotlinaquarium/FirstFragment.kt
+++ app/src/main/java/net/neonlotus/kotlinaquarium/FirstFragment.kt
@@ -173,4 +173,5 @@ class FirstFragment : Fragment() {
         val birthdays = listOf(1, 5, 9, 14, 19, 22, 30)
         return birthdays[Random().nextInt(7)]
     }
+    //trying to get this on github for these test function tracking things
 }
